18918aaf7590f1dfdfd21eb0dc21090367e1c610
ariessandi <ariessandi7@gmail.com>
2020-06-17 06:06:46 +0700

    Updates

---
+++ precache-manifest.5981ca0c28ff1a375a5b6fb8a1b8b6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7823766a565b8df44e8ee606fdf68000",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "39935f9665b53f903858",
+    "url": "/deploy-react/static/css/main.5443197f.chunk.css"
+  },
+  {
+    "revision": "d075f22b993fecb70e38",
+    "url": "/deploy-react/static/js/2.17221783.chunk.js"
+  },
+  {
+    "revision": "a8ee2405eebd59f8275a60a3f531995c",
+    "url": "/deploy-react/static/js/2.17221783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39935f9665b53f903858",
+    "url": "/deploy-react/static/js/main.0f5db094.chunk.js"
+  },
+  {
+    "revision": "0def6e24aa4b79c5836e",
+    "url": "/deploy-react/static/js/runtime-main.adb9fac7.js"
+  }
+]);
